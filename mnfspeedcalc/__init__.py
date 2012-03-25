c280e68ff9ed9caf3e739edcafa9f697640aa331
owenam <aowen@owenam-pc.(none)>
2012-03-25 16:17:10 -0500

    ignore *~

--- mnfspeedcalc/__init__.py
+++ mnfspeedcalc/__init__.py
@@ -1,8 +1,12 @@
 from speedlist import SpeedList
+from trafficreader import TrafficReader
 import impute
 import xml.etree.ElementTree as ET
 
+metro_config = 'test/metro_config.xml'
+
 # load tree from metro_config.xml
+tree = ET.parse(metro_config)
 
 # throw out everything that is not a 'corridor'
 
