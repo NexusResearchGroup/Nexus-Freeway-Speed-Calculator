66eb8e1a65146d7434eef885a3170fee425ba72c
owenam <aowen@owenam-pc.(none)>
2012-03-25 22:39:55 -0500

    - not function yet, main loops are outlined

--- mnfspeedcalc/__init__.py
+++ mnfspeedcalc/__init__.py
@@ -1,9 +1,11 @@
 from speedlist import SpeedList
 from trafficreader import TrafficReader
+from datetime import date, timedelta
 import impute
 import xml.etree.cElementTree as ET
 
 metro_config = 'test/metro_config.xml'
+year = 2010
 
 # load tree from metro_config.xml
 tree = ET.parse(metro_config)
@@ -11,8 +13,29 @@ root = tree.getroot()
 
 corridors = root.findall('corridor')
 
-# for each corridor
+corridor_list = []
 
-#	for each station
+for corridor in corridors:
+	
+	corridor_dict = {}
+	corridor_dict['name'] = corridor.get('route') + ' ' + corridor.get('dir')
 
-#		for each detector
+	station_list = []
+
+	r_nodes = corridor.findall('r_node')
+	for r_node in r_nodes:
+
+		if r_node.get('n_type') == 'Station':
+			station_dict = {}
+			station_dict['station_id'] = r_node.get(station_id)
+			station_dict['speed_limit'] = eval(r_node.get('s_limit'))
+
+			
+			detectors = r_node.findall('detector')
+			for detector in detectors:
+
+
+			station_list.append(station_dict)
+
+	corridor_dict['station_list'] = station_list
+	corridor_list.append(corridor_dict)
