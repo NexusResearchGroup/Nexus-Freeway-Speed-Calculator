009100a3e640a79dd781ffa57fe142eaa60c9dad
Andrew Owen <owenam@gmail.com>
2012-03-30 22:51:04 -0500

    added classes.py, outlines classes for spatial imputation

---
+++ mnfspeedcalc/classes.py
@@ -0,0 +1,39 @@
+from __future__ import division
+
+class Corridor:
+	
+	def __init__(self, name=""):
+		self._name = name
+		self._station_list = []
+		
+	def add_station(self, station):
+		self._station_list.append(station)
+		
+	def stations(self):
+		return self._station_list
+		
+class Station:
+	
+	def __init__(self, id=""):
+		self._id = id
+		self._detector_list = []
+		self._speed_list = []
+		self._speed_limit = -1
+	
+	def add_detector(self, detector):
+		self._detector_list.append(detector)
+	
+	def speeds(self):
+		return self._speed_list
+		
+	def speed_limit(self):
+		return self._speed_limit
+
+class Detector:
+	
+	def __init__(self, id=""):
+		self._id = id
+		self._speed_list = []
+		
+	def speeds(self):
+		return self._speed_list
